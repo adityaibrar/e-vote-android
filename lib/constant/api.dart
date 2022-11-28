f301945bd8a808d6d9f156e0edac42312a7de25b
adityaibrar <adityaibrar@gmail.com>
2022-11-28 11:19:26 +0700

    koneksi dengan web

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -1,5 +1,6 @@
 class AppUrl {
-  static String BaseUrl = "http://10.0.2.2/koneksi.php";
-  static String calon = "$BaseUrl/calon.php";
-  static String user = "$BaseUrl/user.php";
+  static String BaseUrl = "http://10.0.2.2/e-voting/api";
+  static String calon = "$BaseUrl/candidate.php";
+  static String calonDetail = "$BaseUrl/candidateDetail.php";
+  static String user = "$BaseUrl/login.php";
 }

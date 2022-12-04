74e784399485ab4fa358fa45ea14576f138e6de7
adityaibrar <adityaibrar@gmail.com>
2022-12-04 13:12:39 +0700

    created data model

--- lib/model/calon.dart
+++ lib/model/calon.dart
@@ -1,65 +1,53 @@
-// To parse this JSON data, do
-//
-//     final candidate = candidateFromJson(jsonString);
-
 import 'dart:convert';
 
-List<Candidate> candidateFromJson(String str) => List<Candidate>.from(json.decode(str).map((x) => Candidate.fromJson(x)));
+Candidate candidateFromJson(String str) => Candidate.fromJson(json.decode(str));
 
-String candidateToJson(List<Candidate> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String candidateToJson(Candidate data) => json.encode(data.toJson());
 
 class Candidate {
     Candidate({
+        required this.dataCalon,
+    });
+
+    List<DataCalon> dataCalon;
+
+    factory Candidate.fromJson(Map<String, dynamic> json) => Candidate(
+        dataCalon: List<DataCalon>.from(json["data_calon"].map((x) => DataCalon.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "data_calon": List<dynamic>.from(dataCalon.map((x) => x.toJson())),
+    };
+}
+
+class DataCalon {
+    DataCalon({
         required this.idCalon,
         required this.namaCalon,
-        required this.kelas,
-        required this.jurusan,
-        required this.foto,
-        required this.visi,
-        required this.misi,
-        required this.program,
-        required this.ta,
-        required this.periode,
-        required this.status,
+        required this.visimisi,
+        required this.gambar,
+        required this.jumlahVote,
     });
 
     String idCalon;
     String namaCalon;
-    String kelas;
-    String jurusan;
-    String foto;
-    String visi;
-    String misi;
-    String program;
-    String ta;
-    String periode;
-    String status;
+    String visimisi;
+    String gambar;
+    String jumlahVote;
 
-    factory Candidate.fromJson(Map<String, dynamic> json) => Candidate(
+    factory DataCalon.fromJson(Map<String, dynamic> json) => DataCalon(
         idCalon: json["id_calon"],
         namaCalon: json["nama_calon"],
-        kelas: json["kelas"],
-        jurusan: json["jurusan"],
-        foto: json["foto"],
-        visi: json["visi"],
-        misi: json["misi"],
-        program: json["program"],
-        ta: json["TA"],
-        periode: json["periode"],
-        status: json["status"],
+        visimisi: json["visimisi"],
+        gambar: json["gambar"],
+        jumlahVote: json["jumlah_vote"],
     );
 
     Map<String, dynamic> toJson() => {
         "id_calon": idCalon,
         "nama_calon": namaCalon,
-        "kelas": kelas,
-        "jurusan": jurusan,
-        "foto": foto,
-        "visi": visi,
-        "misi": misi,
-        "program": program,
-        "TA": ta,
-        "periode": periode,
-        "status": status,
+        "visimisi": visimisi,
+        "gambar": gambar,
+        "jumlah_vote": jumlahVote,
     };
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,58 +1,69 @@
+// To parse this JSON data, do
+//
+//     final candidate = candidateFromJson(jsonString);
 
 import 'dart:convert';
 
-User userFromJson(String str) => User.fromJson(json.decode(str));
+Candidate candidateFromJson(String str) => Candidate.fromJson(json.decode(str));
 
-String userToJson(User data) => json.encode(data.toJson());
+String candidateToJson(Candidate data) => json.encode(data.toJson());
 
-class User {
-  User({
-    required this.result,
-  });
+class Candidate {
+    Candidate({
+        required this.code,
+        required this.status,
+        required this.dataUser,
+    });
 
-  Result result;
+    int code;
+    String status;
+    List<DataUser> dataUser;
 
-  factory User.fromJson(Map<String, dynamic> json) => User(
-        result: Result.fromJson(json["result"]),
-      );
+    factory Candidate.fromJson(Map<String, dynamic> json) => Candidate(
+        code: json["code"],
+        status: json["status"],
+        dataUser: List<DataUser>.from(json["data_user"].map((x) => DataUser.fromJson(x))),
+    );
 
-  Map<String, dynamic> toJson() => {
-        "result": result.toJson(),
-      };
+    Map<String, dynamic> toJson() => {
+        "code": code,
+        "status": status,
+        "data_user": List<dynamic>.from(dataUser.map((x) => x.toJson())),
+    };
 }
 
-class Result {
-  Result({
-    required this.idUser,
-    required this.nama,
-    required this.username,
-    required this.password,
-    required this.akses,
-    required this.joiningDate,
-  });
+class DataUser {
+    DataUser({
+        required this.idUser,
+        required this.nama,
+        required this.username,
+        required this.password,
+        required this.akses,
+        required this.joiningDate,
+    });
 
-  String idUser;
-  String nama;
-  String username;
-  String password;
-  String akses;
-  DateTime joiningDate;
+    String idUser;
+    String nama;
+    String username;
+    String password;
+    String akses;
+    DateTime joiningDate;
 
-  factory Result.fromJson(Map<String, dynamic> json) => Result(
+    factory DataUser.fromJson(Map<String, dynamic> json) => DataUser(
         idUser: json["id_user"],
         nama: json["nama"],
         username: json["username"],
         password: json["password"],
         akses: json["akses"],
         joiningDate: DateTime.parse(json["joining_date"]),
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "id_user": idUser,
         "nama": nama,
         "username": username,
         "password": password,
         "akses": akses,
         "joining_date": joiningDate.toIso8601String(),
-      };
+    };
 }
